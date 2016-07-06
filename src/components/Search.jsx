b797a710651f27a337333e9ad6c1a881fd2caa4b
zach.jung <zach.jung@kakaocorp.com>
2016-07-06 13:43:12 +0900

    init

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import {render} from 'react-dom';
+import Promptor from './Promptor.jsx';
 
 
 export default class Search extends Component {
+	constructor(){
+		super();
+		this.state = {
+			searchTerm: "React"
+		};
+	}
+
+	handleChange(event){
+		this.setState({searchTerm: event.target.value});
+		render(<Promptor output = {this.state.searchTerm}/>, document.getElementById('subRoot'));
+	}
+
 	render(){
 		return (
 			<div>
-				Search Term: <input type="search" value="React"/>
+				Search Term: <input type="search" value={this.state.searchTerm} onChange={this.handleChange.bind(this)}/>
 			</div>
 		);
 	}
--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { render } from 'react-dom';
 import Search from './Search.jsx';
 
 
-render(<Search/>, document.getELementById('root'));
+render(<Search/>, document.getElementById('root'));
